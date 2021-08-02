c8bfbd7fa1ba7f38a439dfc964f59d995ed24669
ryuji.tamakawa <homes31285@gmail.com>
2021-08-02 14:24:50 +0000

    second commit

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,6 +1,5 @@
-LastName = '玉川'
-FirstName='隆二'
+last_name = '玉川'
+first_name='隆二'
 age = 27
  
-puts LastName + FirstName + 'です。'
-puts  age.to_s + '歳です。'
+puts last_name + first_name + 'です。'+ age.to_s + '歳です。'
